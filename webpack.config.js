679e3dcc74f3d99501b10f55cc16ba64d18c268e
Andrzej Bogdanowicz <andrzej.bogdanowicz@rtbhouse.com>
2018-11-04 17:11:23 +0100

    Content

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/app.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [{
+        test: /\.(t|j)sx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader
+        }, "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    })
+  ],
+  devtool: "source-map",
+  stats: 'minimal',
+  devServer: {
+    stats: 'minimal'
+  }
+}
